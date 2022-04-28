3327602c0f4ee96171f25b5b8e06553b80489a9a
rmartinez10 <45635549+rmartinez10@users.noreply.github.com>
2022-04-28 17:38:52 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -4,17 +4,18 @@ print('==================<<<   C A L C U L A D O R A   >>>==================')
 print(' ')
 print(' ')
 number_1 = int(input('Enter your first number.: '))
-number_2 = int(input('Enter your second number: '))
 operacao = input('''Entre com a operação....: ''')
-print(' ')
-print(' ')
+number_2 = int(input('Enter your second number: '))
+
+print('calculando...')
+print('calculando...')
 if   operacao == '+':
    result = number_1 + number_2
 elif operacao == '-':
    result = number_1 - number_2    
 elif operacao == '*':
    result = number_1 * number_2       
-elif operacao == '/':
+elif operacao == '/' or operacao == '%':
    result = number_1 / number_2       
 
 print('O resultado da operação é: ',result)
